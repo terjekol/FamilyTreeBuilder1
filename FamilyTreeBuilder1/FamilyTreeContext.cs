14bf689dfdd715befed134697f7892633f80dee2
Terje Kolderup <terje@kolderup.net>
2019-07-31 09:13:56 +0200

    x

---
+++ FamilyTreeBuilder1/FamilyTreeContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FamilyTreeBuilder1
+{
+    public partial class FamilyTreeContext : DbContext
+    {
+        public FamilyTreeContext()
+        {
+        }
+
+        public FamilyTreeContext(DbContextOptions<FamilyTreeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> Person { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("host=localhost;database=FamilyTree;user id=postgres;password=abcd1234");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.ToTable("person");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Birthdate)
+                    .HasColumnName("birthdate")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Deathdate)
+                    .HasColumnName("deathdate")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Father).HasColumnName("father");
+
+                entity.Property(e => e.Firstname)
+                    .HasColumnName("firstname")
+                    .HasColumnType("character varying");
+
+                entity.Property(e => e.Lastname)
+                    .HasColumnName("lastname")
+                    .HasColumnType("character varying");
+
+                entity.Property(e => e.Mother).HasColumnName("mother");
+
+                entity.HasOne(d => d.FatherNavigation)
+                    .WithMany(p => p.InverseFatherNavigation)
+                    .HasForeignKey(d => d.Father)
+                    .HasConstraintName("person_father_fkey");
+
+                entity.HasOne(d => d.MotherNavigation)
+                    .WithMany(p => p.InverseMotherNavigation)
+                    .HasForeignKey(d => d.Mother)
+                    .HasConstraintName("person_mother_fkey");
+            });
+        }
+    }
+}
---
+++ FamilyTreeBuilder1/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyTreeBuilder1
+{
+    public partial class Person
+    {
+        public Person()
+        {
+            InverseFatherNavigation = new HashSet<Person>();
+            InverseMotherNavigation = new HashSet<Person>();
+        }
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public DateTime? Deathdate { get; set; }
+        public int? Father { get; set; }
+        public int? Mother { get; set; }
+
+        public virtual Person FatherNavigation { get; set; }
+        public virtual Person MotherNavigation { get; set; }
+        public virtual ICollection<Person> InverseFatherNavigation { get; set; }
+        public virtual ICollection<Person> InverseMotherNavigation { get; set; }
+    }
+}
